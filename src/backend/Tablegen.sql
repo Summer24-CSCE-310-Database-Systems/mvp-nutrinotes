a1e0840479ca2b2e9c8fe3ffc6c29fa831eafe64
dobylane <dlanete@tamu.edu>
2024-07-24 13:56:38 -0500

    Docker Files
    This contains the docker file that creates the container that will hold the database. As well as the SQL query that generates the tables

---
+++ src/backend/Tablegen.sql
@@ -0,0 +1,39 @@
+-- Users table
+CREATE TABLE Users (
+    User_ID SERIAL PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL
+);
+
+-- Food table
+CREATE TABLE Food (
+    Food_ID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Calories INT NOT NULL
+);
+
+-- Catalog table
+CREATE TABLE Catalog (
+    Catalog_ID SERIAL PRIMARY KEY,
+    User_ID INT NOT NULL,
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
+);
+
+-- Serving table (Join Table)
+CREATE TABLE Serving (
+    Catalog_ID INT NOT NULL,
+    Food_ID INT NOT NULL,
+    Name VARCHAR(255),
+    PRIMARY KEY (Catalog_ID, Food_ID),
+    FOREIGN KEY (Catalog_ID) REFERENCES Catalog(Catalog_ID),
+    FOREIGN KEY (Food_ID) REFERENCES Food(Food_ID)
+);
+
+-- Goals table
+CREATE TABLE Goals (
+    Goal_ID SERIAL PRIMARY KEY,
+    User_ID INT NOT NULL,
+    Weight FLOAT NOT NULL,
+    Date_of_Goal DATE NOT NULL,
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
+);
