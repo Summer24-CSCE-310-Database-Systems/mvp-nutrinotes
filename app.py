a7631c7e2dbbb57f1e022f82b362a8bbacbaba71
dobylane <dlanete@tamu.edu>
2024-07-25 19:35:14 -0500

    Create User + error checking

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'Users'
     User_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Username = db.Column(db.String(255), nullable=False)
+    Username = db.Column(db.String(255), unique=True, nullable=False)
     Password = db.Column(db.String(255), nullable=False)
     catalogs = db.relationship('Catalog', back_populates='user')
     goals = db.relationship('Goal', back_populates='user')
@@ -24,7 +24,7 @@ class User(db.Model):
 class Food(db.Model):
     __tablename__ = 'Food'
     Food_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Name = db.Column(db.String(255), nullable=False)
+    Name = db.Column(db.String(255), unique=True, nullable=False)
     Calories = db.Column(db.Integer, nullable=False)
     servings = db.relationship('Serving', back_populates='food')
 
@@ -57,4 +57,29 @@ if __name__ == '__main__':
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route("/user")
+def userinfo(feedback_message=None, feedback_type=False):
+    return render_template("user.html",
+            feedback_message=feedback_message, 
+            feedback_type=feedback_type)
+
+@app.route("/usercreate", methods=['POST'])
+def usercreate():
+    Username = request.form["Username"]
+    Password = request.form["Password"]
+
+    try:
+        entry = User(Username=Username, Password=Password)
+        db.session.add(entry)
+        db.session.commit()
+    except exc.IntegrityError as err:
+        db.session.rollback()
+        return userinfo(feedback_message='A username named {} already exists. Create a username with a different name.'.format(Username), feedback_type=False)
+    except Exception as err:
+        db.session.rollback()
+        return userinfo(feedback_message='Database error: {}'.format(err), feedback_type=False)
+    
+    return userinfo(feedback_message='Successfully added chef {}'.format(Username),
+                       feedback_type=True)
 
