0fe16e60ead9b11b742e30c47a1186e084a66508
ewenthe-tamu <ethan.wenthe@gmai.com>
2024-07-30 18:38:50 -0500

    Create a goal Works

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 app = Flask(__name__, template_folder='src/frontend/templates')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<user>:<password>@localhost/<appname>'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/nutrinotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Little12@localhost/nutrinotes'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
 login_manager = LoginManager()
@@ -694,3 +694,61 @@ def is_valid_date(date_str, date_format='%Y-%m-%d'):
         return True
     except ValueError:
         return False
+
+
+#CRUD FOR Goals
+@app.route("/goals")
+def goals():
+    return render_template('goals.html')
+
+#CREATE
+@app.route("/creategoal", methods=['POST'])
+def creategoal():
+    Username = request.form['username']
+    Weight = request.form.get("Weight")
+    Date_of_Goal = request.form.get("Date_of_Goal")
+
+    if not Username or not Weight or not Date_of_Goal:
+        send = "Please Enter All Information!"
+        return render_template('goals.html', feedback_message=send, feedback_type=False)
+    try:
+        user = db.session.query(User).filter(User.Username == Username).first()
+        if user is None:
+            send = 'USER ID NOT FOUND ({})'.format(Username)
+            return render_template('goals.html', feedback_message=send, feedback_type=False)
+        
+        newGoal = Goal(
+            User_ID=user.User_ID,
+            Weight=float(Weight),
+            Date_of_Goal=Date_of_Goal
+        )
+
+        db.session.add(newGoal)
+        db.session.commit()
+
+        send = 'Goal Added successfully!'
+        return render_template('goals.html', feedback_message=send, feedback_type=True)
+    except Exception as err:
+        db.session.rollback()
+        return render_template('goals.html', feedback_message=str(err), feedback_type=False)
+
+#DELETE
+#@app.route("/deletegoal/<int:goal_id>", methods=['POST'])
+#def deletegoal(goal_id):
+#    try:
+#        goal = db.session.query(Goals).filter(Goals.Goal_ID == goal_id).first()
+#        if goal is None:
+#            send = 'Goal ID NOT FOUND ({})'.format(goal_id)
+#            return render_template('goals.html', feedback_message=send, feedback_type=False)
+#        
+#        db.session.delete(goal)
+#        db.session.commit()
+#
+##        send = 'Goal Deleted successfully!'
+#        return redirect(url_for('goals', feedback_message=send, feedback_type=True))
+#    except Exception as err:
+#        db.session.rollback()
+#        return render_template('goals.html', feedback_message=str(err), feedback_type=False)
+#
+if __name__ == "__main__":
+    app.run(debug=True)
